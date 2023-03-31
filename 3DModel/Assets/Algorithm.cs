a52c281f9698d27e2a8673b93714e862050788d7
Peter Kocew <petarkocew@gmail.com>
2023-03-31 12:30:12 +0100

    Move files and delete old ones

---
+++ 3DModel/Assets/Algorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Algorithm: MonoBehaviour, AlgorithmInterface
+{
+    // for position step
+    public int updateCounter = 0;
+    public int numOfIterations;
+
+
+    public abstract void OptimizationStep();
+
+    public abstract void Start();
+
+    public abstract void Update();
+}
---
+++ 3DModel/Assets/SensorObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SensorObject : MonoBehaviour
+{
+    public object value;
+
+    public float[] positions;
+
+    public float[] orientations;
+    public abstract void printAreas();
+    public abstract void updateArea();
+}
